3aa14ac2d77187e573dda99f91ee9c0224bde4da
Hassan Hamidi <50262458+hahamidi@users.noreply.github.com>
2023-01-26 00:16:35 +0330

    1

---
+++ ml3d/torch/run_kp.py
@@ -0,0 +1,30 @@
+import sys
+
+import ml3d.torch as ml3d
+
+import open3d.ml as _ml3d
+import open3d.ml.torch as ml3d
+from ml3d.utils.config import Config
+from ml3d.torch.models.randlanet import RandLANet
+from ml3d.torch.models.kpconv import KPFCNN
+from ml3d.datasets.s3dis import S3DIS
+from  ml3d.torch.pipelines.semantic_segmentation import SemanticSegmentation
+import warnings
+warnings.filterwarnings("ignore")
+
+
+# Make some changes to the open3d.ml.torch module
+
+# Reload the module
+
+
+
+# cfg_file = "/content/OML/ml3d/configs/randlanet_s3dis.yml"
+cfg_file = "/content/OML/ml3d/configs/kpconv_s3dis.yml"
+cfg = Config.load_from_file(cfg_file)
+
+model = KPFCNN(**cfg.model)
+cfg.dataset['dataset_path'] = "/content/OML/Stanford3dDataset_v1.2_Aligned_Version"
+dataset = S3DIS(cfg.dataset.pop('dataset_path', None), **cfg.dataset)
+pipeline = SemanticSegmentation(model, dataset=dataset, device="gpu", **cfg.pipeline)
+pipeline.run_train()
